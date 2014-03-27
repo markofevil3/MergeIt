2d4e0cb2e8290f867a49cbaac2dff44e629b76b7
Bui Phi Quan <rongxanh2052004@yahoo.com>
2014-03-27 22:21:52 +0700

    fix bugs

--- Assets/Scripts/Popup/ResultPopup.cs
+++ Assets/Scripts/Popup/ResultPopup.cs
@@ -32,13 +32,17 @@ public class ResultPopup : Popup {
 		highestTile = (int)data[1];
 		bool isHighScore = (bool)data[2];
     if (isHighScore) {
+      
       title.spriteName = "txt_newHighScore";
       highScoreStar.SetActive(true);
       if (ScreenManager.internetAvailable) {
         #if UNITY_IPHONE
+          var permissions = new string[] { "publish_actions", "publish_stream" };
+    			FacebookBinding.reauthorizeWithPublishPermissions( permissions, FacebookSessionDefaultAudience.Everyone );
+    			
           var parameters = new Dictionary<string,string>
           {
-            { "link", "https://itunes.apple.com/us/app/power-of-2/id841898323?ls=1&mt=8" },
+            { "link", "http://www.google.com" },
             { "name", "Power of 2" },
             { "picture", "https://dl.dropboxusercontent.com/u/86872228/PowerOf2/logo.png" },
             { "caption", "I got " + score + " points! Can you beat my score?" }
@@ -89,7 +93,7 @@ public class ResultPopup : Popup {
       #if UNITY_IPHONE
         var parameters = new Dictionary<string,string>
         {
-          { "link", "https://itunes.apple.com/us/app/power-of-2/id841898323?ls=1&mt=8" },
+          { "link", "http://www.google.com" },
           { "name", "Power of 2" },
           { "picture", "https://dl.dropboxusercontent.com/u/86872228/PowerOf2/logo.png" },
           { "caption", "I got " + score + " points! Can you beat my score?" }
